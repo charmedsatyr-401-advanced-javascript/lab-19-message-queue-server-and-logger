f561cd047b8d0aca5909eea430855f7ebd97d378
Joseph <z@charmed.tech>
2019-04-11 16:22:24 -0700

    Implement full db logger

---
+++ logger.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const Q = require('@nmq/q/client');
+Q.start();
+
+const files = new Q('files');
+const db = new Q('database');
+
+// Subscribe to `files` events
+files.subscribe('create', payload => console.log('create in `files`:', payload));
+files.subscribe('read', payload => console.log('read in `files`:', payload));
+files.subscribe('update', payload => console.log('update in `files`:', payload));
+files.subscribe('delete', payload => console.log('delete in `files`:', payload));
+
+// Subscribe to `db` events
+db.subscribe('save', payload => console.log('save in `db`:', payload));
+db.subscribe('error', payload => console.log('error in `db`:', payload));
